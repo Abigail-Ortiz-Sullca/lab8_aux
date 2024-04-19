06f9e2b7179e3a0ef74e10e3862413c33a122327
OrtizAbigail <amoscma469@gmail.com>
2024-04-19 19:50:08 -0400

    feature(Q-2): increment counter by 5

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const Button = () => {
+  const [value, setValue] = useState(5);
+
+  const handleClick = () => {
+    setValue(value + 50);
+  };
+
+  return (
+    <button onClick={handleClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Button;
+
+
+
+
+
